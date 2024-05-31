65c54ef9ae495633212530d598f735af12ef25a0
SAID23S23988 <154741482+SAID23S23988@users.noreply.github.com>
2024-05-31 23:55:43 +0400

    Update NotificationReceiver.java

--- app/src/main/java/com/example/myapp/NotificationReceiver.java
+++ app/src/main/java/com/example/myapp/NotificationReceiver.java
@@ -1,10 +1,13 @@
+
 package com.example.myapp;
+
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
 import androidx.core.app.NotificationCompat;
 import androidx.core.content.ContextCompat;
 
